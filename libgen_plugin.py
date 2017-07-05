389d787c645d0b42db4bd182d9a861e5a2ddf9bb
MCOfficer <mcofficer@gmx.de>
2017-07-05 13:51:25 +0200

    minor beautifying

--- libgen_plugin.py
+++ libgen_plugin.py
@@ -23,7 +23,7 @@ class LibGen_Store(BasicStoreConfig, StorePlugin):
 
     def search(self, query, max_results=10, timeout=60):
 
-        lg=libgenapi.Libgenapi(["http://libgen.io","http://gen.lib.rus.ec","http://93.174.95.27/","http://libgen.in","http://libgen.org"])
+        lg = libgenapi.Libgenapi(["http://libgen.io", "http://gen.lib.rus.ec", "http://93.174.95.27/", "http://libgen.in", "http://libgen.org"])
         try:
             results = lg.search(query)
             abort = False
@@ -46,7 +46,7 @@ class LibGen_Store(BasicStoreConfig, StorePlugin):
                 s.drm = SearchResult.DRM_UNLOCKED
                 extension = r['extension']
 
-                #prep cover
+                # prep cover
                 coverpage = 'http://gen.lib.rus.ec' + r['mirrors'][1]
                 coverpage = coverpage.replace('ads.', 'book/index.')
                 with closing(br.open(coverpage, timeout=10)) as f:
@@ -57,19 +57,19 @@ class LibGen_Store(BasicStoreConfig, StorePlugin):
                 cover = 'http://gen.lib.rus.ec' + cover
                 s.cover_url = cover
 
-                #prep download via libgen
+                # prep download via libgen
                 lgpage = 'http://libgen.io' + r['mirrors'][1]
                 with closing(br.open(lgpage, timeout=10)) as f:
                     doc = f.read()
                 linkend = doc.find('><h2>DOWNLOAD') - 1
-                doc = doc[linkend-100:linkend]
+                doc = doc[linkend - 100:linkend]
                 linkpos = doc.find('http')
                 libgendl = doc[linkpos:linkend]
                 libgendl = libgendl.replace('amp;', '')
                 libgenformat = 'libgen: .' + extension
                 s.downloads[libgenformat] = libgendl
 
-                #get download location from libgen.pw
+                # get download location from libgen.pw
                 lgpw = r['mirrors'][0].replace('view', 'download')
                 with closing(br.open(lgpw, timeout=10)) as f:
                     doc = f.read()
@@ -77,18 +77,18 @@ class LibGen_Store(BasicStoreConfig, StorePlugin):
                 end = doc.find('status') - 27
                 location = doc[pos:end]
 
-                #prep filename
+                # prep filename
                 filename = r['author'] + ' - ' + r['title']
                 filename = filename.replace(' ', '_')
                 filename = filename.encode('utf8')
                 filename = urllib2.quote(filename)
 
-                #prep download via b-ok (/bookzz/boosc/bookza)
+                # prep download via b-ok (/bookzz/boosc/bookza)
                 bokdl = 'http://dlx.b-ok.org/genesis/' + location + '/_as/' + filename + '.pdf'
                 bokformat = 'b-ok: .' + extension
                 s.downloads[bokformat] = bokdl
 
-                #prep download via bookfi
+                # prep download via bookfi
                 bookfidl = 'http://dl.lux.bookfi.net/genesis/' + location + '/_as/' + filename + '.pdf'
                 bookfiformat = 'bookfi: .' + extension
 #                s.downloads[bookfiformat] = bookfidl
